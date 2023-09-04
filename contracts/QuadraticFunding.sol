d38457f531848bcfedeb9a6cdca5b5b67c01ddce
Kouga Ishizawa <7k7877@gmail.com>
2023-09-05 00:08:11 +0900

    🚀 / GovContract with Interface / deployed functions.

--- contracts/QuadraticFunding.sol
+++ contracts/QuadraticFunding.sol
@@ -1,31 +1,36 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import "./Type.sol";
-import "@openzeppelin/utils/math/Math.sol";
-import "@openzeppelin/utils/structs/EnumerableMap.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {EnumerableMap} from "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 
 
-contract QuadraticFunding is Type {
+contract QuadraticFunding {
 
     using Math for uint256;
     using EnumerableMap for EnumerableMap.AddressToUintMap;
 
+    struct ProjectValue {
+        uint256 contributors;
+        uint256 avgDonation;
+        uint256 matchAmount;
+    }
+
     // amount of matching pool
-    EnumerableMap.AddressToUintMap internal matchingPoolAmount;
+    EnumerableMap.AddressToUintMap internal _matchingPoolAmount;
 
     // calculate the power of a project.
-    function _calcPower(Project calldata _project) pure internal returns(uint256) {
+    function _calcPower(ProjectValue calldata _project) pure internal returns(uint256) {
         return (_project.avgDonation.sqrt()*_project.contributors)**2;
     }
 
-    function _calcAvg(uint256 _donation, Project calldata _project) pure internal returns(uint256) {
+    function _calcAvg(ProjectValue calldata _project, uint256 _donation) pure internal returns(uint256) {
         return _donation.average(_project.avgDonation);
     }
 
-    // calculate a matched amount from the share pool.
-    function _calcMatchingAmount(Project[] calldata projects, uint256 _id) view internal returns(uint256) {
-        assert(projects[_id].isProposed && projects[_id].contributors != 0);
+    // calculate a matched amount from the pool.
+    function _calcMatchingAmount(ProjectValue[] calldata projects, uint256 _id) view internal returns(uint256) {
+        assert(projects[_id].contributors != 0);
 
         uint256 _totalPower = 0;
         for(uint256 i = 0; i < projects.length; i++) {
@@ -40,13 +45,12 @@ contract QuadraticFunding is Type {
     }
 
     function _getPoolAmount() view internal returns(uint256) {
-        uint256 _total = 0;
+        uint256 _totalAmount = 0;
         for(uint256 i = 0; i < matchingPoolAmount.length(); i++) {
             (address sponsor, ) = matchingPoolAmount.at(i);
-            _total += matchingPoolAmount.get(sponsor);
+            _totalAmount += matchingPoolAmount.get(sponsor);
         }
-        return _total;
+        return _totalAmount;
     }
-
 
 }
